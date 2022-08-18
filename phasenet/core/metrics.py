aba591c2c3957b77c13791c242209ae4e241b768
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2022-08-18 18:52:15 -0400

    FIx the bug in AUC

--- phasenet/core/metrics.py
+++ phasenet/core/metrics.py
@@ -147,9 +147,9 @@ class AUC(Metric):
         fpr = []
         for idx, _ in enumerate(self.thresholds):
             tpr.append((self.tp[idx].float())/(self.tp[idx]+self.fn[idx])
-                       ) if self.tp[idx]+self.fn[idx] != 0 else torch.tensor(0.0)
+                       if self.tp[idx]+self.fn[idx] != 0 else torch.tensor(0.0))
             fpr.append((self.fp[idx].float())/(self.fp[idx]+self.tn[idx])
-                       ) if self.fp[idx]+self.tn[idx] != 0 else torch.tensor(0.0)
+                       if self.fp[idx]+self.tn[idx] != 0 else torch.tensor(0.0))
         tpr = [each.item() for each in tpr]
         fpr = [each.item() for each in fpr]
         return torch.trapezoid(torch.tensor(tpr[::-1]), torch.tensor(fpr[::-1]))
