d744d5942802febb3af9ced3243ba964e8e1b532
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2022-08-20 18:20:18 -0400

    Update test

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ warnings.filterwarnings(
     "ignore", ".*During `trainer.test()`, it is recommended to use `Trainer(devices=1)`*")
 
 
-@hydra.main(config_path="phasenet/conf", config_name="config")
+@hydra.main(config_path="phasenet/conf", config_name="config", version_base="1.2")
 def test_app(cfg: Config) -> None:
     train_conf = cfg.train
     # * current version
